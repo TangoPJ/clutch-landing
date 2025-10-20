ba542501c2a9f03b41ad116276c5327384e5dff2
tangopj <rail.batyrshin.js@gmail.com>
2025-10-20 13:41:44 +0600

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from "astro/config";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
+
+  server: {
+    open: true,
+  },
+
+  adapter: vercel(),
+});
